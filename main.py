d0643499cf7406e0c5db0b28ea4b415c592476fb
Marko Ristic <marko@3ap.ch>
2025-02-16 22:42:25 +0100

    init solution

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
+import json
 
 load_dotenv(override=True)
 openai_api_key = os.getenv('OPENAI_API_KEY')
@@ -17,21 +18,69 @@ openai = OpenAI()
 MODEL = 'gpt-4o-mini'
 
 system_message = "You are a helpful assistant and sales expert in a clothes store. You should try to gently encourage \
-the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. \
-For example, if the customer says 'I'm looking to buy a hat', \
-you could reply something like, 'Wonderful - we have lots of hats - including several that are part of our sales event.'\
-Encourage the customer to buy hats if they are unsure what to get."
-system_message += "\nIf the customer asks for shoes, you should respond that shoes are not on sale today, \
-but remind the customer to look at hats!"
+the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. "
+system_message += "Always use a Serbia language in communication."
+system_message += "Always use a Serbian dinar as a currency"
+system_message += "Always be accurate. If you don't know the answer, say so."
+
+items_prices = {
+    "kapa": 60,
+    "kape": 60,
+    "cipela": 200,
+    "cipele": 200,
+    "kravata": 15,
+    "kravate": 15,
+    "majica": 25,
+    "majice": 25,
+}
+
+def get_item_price(shop_item_name):
+    item = shop_item_name.lower()
+    return items_prices.get(shop_item_name, "Unknown")
+  
+price_function = {
+    "name": "get_item_price",
+    "description": "Get the price of a shop item. Call this whenever you need to know the item price, for example when a customer asks 'How much is a hat?'",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "shop_item_name": {
+                "type": "string",
+                "description": "The name of the shop product item",
+            },
+        },
+        "required": ["shop_item_name"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": price_function}]
 
 def chat(message, history):
     messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools)
 
-    stream = openai.chat.completions.create(model=MODEL, messages=messages, stream=True)
+    if response.choices[0].finish_reason=="tool_calls":
+        message = response.choices[0].message
+        response, item = handle_tool_call(message)
+        messages.append(message)
+        messages.append(response)
+        response = openai.chat.completions.create(model=MODEL, messages=messages)
+    
+    return response.choices[0].message.content
+  
+def handle_tool_call(message):
+    tool_call = message.tool_calls[0]
+    arguments = json.loads(tool_call.function.arguments)
+    print(arguments)
+    item = arguments.get('shop_item_name')
 
-    response = ""
-    for chunk in stream:
-        response += chunk.choices[0].delta.content or ''
-        yield response
+    price = get_item_price(item)
+    response = {
+        "role": "tool",
+        "content": json.dumps({"item": item,"price": price}),
+        "tool_call_id": tool_call.id
+    }
+    return response, item
 
 gr.ChatInterface(fn=chat, type="messages").launch(inbrowser=True)
